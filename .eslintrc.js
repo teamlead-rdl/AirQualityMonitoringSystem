361fe2e5bed12373ee70a3575ae7cb3f2d537eb5
Prajwalkodmannu <103936678+Prajwalkodmannu@users.noreply.github.com>
2022-04-21 16:26:30 +0530

    ESLint implementation (#7)
    * Bug fixes in Milestone 1
    
    * ESLint Changes
    
    Co-authored-by: shishirkumarm <nofear.shishir@gmail.com>
    Co-authored-by: Ranjith-K-07 <ranjith@rdltech.in>
    Co-authored-by: Prajwal <prajwal@udemy.com>

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+  },
+};
