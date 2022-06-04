42595024c3c5e732dc21f4ce3fcab3f3e714ada0
Ranjith-K-07 <ranjith@rdltech.in>
2022-06-04 15:29:53 +0530

    Modified Dashboard Layout design

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './dashboard/dragResize.scss';
 import { Grid } from '@mui/material';
 import LocationGridWidget from './dashboard/components/LocationGridWidget';
@@ -7,39 +7,62 @@ import GeoLocationWidget from './dashboard/components/GeoLocationWidget';
 /* eslint-disable no-unused-vars */
 function Dashboard() {
   const [locationDetails, setLocationDetails] = useState({
-    location_id : '',
+    location_id: '',
     branch_id: '',
     facility_id: '',
     building_id: '',
     floor_id: '',
-    lab_id: ''
+    lab_id: '',
   });
 
   const [locationState, setProgressState] = useState(0);
 
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={8}>
-        <Grid container item xs={12}>
-          <Grid item xs={12} sx={{
-            marginTop: 1,
-            backgroundColor: 'skyblue'
-          }}>
-            <LocationGridWidget locationDetails={locationDetails} setLocationDetails={setLocationDetails} locationState={locationState} setProgressState={setProgressState} />
+    <Grid container spacing={1} style={{ height: '92.5%' }}>
+      <Grid
+        item
+        xs={12}
+        style={{ height: '50%' }}
+        sx={{
+          marginLeft: 1,
+        }}
+      >
+        <Grid
+          container
+          item
+          xs={12}
+          style={{ height: '100%' }}
+        >
+          <Grid
+            item
+            xs={8}
+            sx={{
+            }}
+            style={{ height: '100%' }}
+          >
+            <LocationGridWidget
+              locationDetails={locationDetails}
+              setLocationDetails={setLocationDetails}
+              locationState={locationState}
+              setProgressState={setProgressState}
+            />
           </Grid>
 
-          <Grid item xs={12} sx={{
-            backgroundColor: 'pink'
-          }}>
-            <AlertWidget/>
+          <Grid item xs={4}>
+            <GeoLocationWidget />
           </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={4} sx={{
-        backgroundColor: 'pink',
-        marginTop: 2
-      }}>
-        <GeoLocationWidget/>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          padding: 1,
+          marginLeft: 1,
+        }}
+        style={{ height: '50%' }}
+      >
+        <AlertWidget />
       </Grid>
     </Grid>
   );
--- src/components/dashboard/subComponent/siteDetailsComponent/BuildingGridComponent.js
+++ src/components/dashboard/subComponent/siteDetailsComponent/BuildingGridComponent.js
@@ -1,9 +1,13 @@
 import { Breadcrumbs, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { BuildingFetchService } from '../../../../services/LoginPageService';
-
-const BuildingGridComponent = ({locationDetails, setLocationDetails, setProgressState, breadCrumbLabels, setBreadCrumbLabels}) => {
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+function BuildingGridComponent({
+  locationDetails, setLocationDetails, setProgressState, breadCrumbLabels, setBreadCrumbLabels,
+}) {
   const dataColumns = [
     {
       field: 'buildingName',
@@ -27,41 +31,41 @@ const BuildingGridComponent = ({locationDetails, setLocationDetails, setProgress
   ];
   const [dataList, setDataList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     BuildingFetchService({
       location_id: locationDetails.location_id,
       branch_id: locationDetails.branch_id,
       facility_id: locationDetails.facility_id,
     }, handleSuccess, handleException);
-  },[locationDetails]);
+  }, [locationDetails]);
 
   const handleSuccess = (dataObject) => {
     setDataList(dataObject.data);
-  }
+  };
 
   const handleException = (errorObject) => {
   };
 
-  const LinkTo = ({selectedRow}) =>{
+  function LinkTo({ selectedRow }) {
     return (
-      <h3 onClick={()=>{
-        setLocationDetails((oldValue)=>{
-          return {...oldValue, building_id: selectedRow.id};
+      <h3 onClick={() => {
+        setLocationDetails((oldValue) => {
+          return { ...oldValue, building_id: selectedRow.id };
         });
 
-        setBreadCrumbLabels((oldvalue)=>{
-          return { ...oldvalue, buildingLabel: selectedRow.buildingName}
+        setBreadCrumbLabels((oldvalue) => {
+          return { ...oldvalue, buildingLabel: selectedRow.buildingName };
         });
 
         setProgressState(4);
-      }}>
+      }}
+      >
         {selectedRow.buildingName}
       </h3>
-    )
+    );
   }
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      BuildingGridComponent
+    <div style={{ height: '100%', width: '100%' }}>
       <Breadcrumbs aria-label="breadcrumb" separator="›">
         <h3>
           Location
@@ -85,10 +89,10 @@ const BuildingGridComponent = ({locationDetails, setLocationDetails, setProgress
         pageSize={5}
         rowsPerPageOptions={[5]}
         disableSelectionOnClick
-        style={{ maxHeight: `${80}%` }}
+        style={{ maxHeight: `${93}%` }}
       />
-      </div>
-  )
+    </div>
+  );
 }
 
-export default BuildingGridComponent
+export default BuildingGridComponent;
