0d60e8c069aece2cf40c6b9d8f8bf84a087a785e
shishirkumarm <nofear.shishir@gmail.com>
2022-06-14 01:36:06 +0530

    uninstall nivo chart and install chartJs

--- src/components/dashboard/subComponent/landingPageComponents/SensorGraphComponent.js
+++ src/components/dashboard/subComponent/landingPageComponents/SensorGraphComponent.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { ResponsiveBar } from '@nivo/bar';
 import {
   Button, Dialog, DialogContent, DialogTitle, TextField,
 } from '@mui/material';
@@ -95,119 +94,7 @@ function SensorGraphComponent({
           </Grid>
         </Grid>
         <div style={{ height: 250 }}>
-          <ResponsiveBar
-            data={data}
-            keys={[
-              'y',
-            ]}
-            indexBy="x"
-            margin={{
-              top: 50, right: 130, bottom: 50, left: 60,
-            }}
-            padding={0.3}
-            valueScale={{ type: 'linear' }}
-            indexScale={{ type: 'band', round: true }}
-            colors={{ scheme: 'nivo' }}
-            defs={[
-              {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: 'red',
-                size: 4,
-                padding: 1,
-                stagger: true,
-              },
-              {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: 'red',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10,
-              },
-            ]}
-            fill={[
-              {
-                match: {
-                  id: 'fries',
-                },
-                id: 'dots',
-              },
-              {
-                match: {
-                  id: 'sandwich',
-                },
-                id: 'lines',
-              },
-            ]}
-            borderColor={{
-              from: 'color',
-              modifiers: [
-                [
-                  'darker',
-                  1.6,
-                ],
-              ],
-            }}
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legend: 'x',
-              legendPosition: 'middle',
-              legendOffset: 32,
-            }}
-            axisLeft={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legend: 'Value',
-              legendPosition: 'middle',
-              legendOffset: -40,
-            }}
-            labelSkipWidth={12}
-            labelSkipHeight={12}
-            labelTextColor={{
-              from: 'color',
-              modifiers: [
-                [
-                  'darker',
-                  1.6,
-                ],
-              ],
-            }}
-            legends={[
-              {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                  {
-                    on: 'hover',
-                    style: {
-                      itemOpacity: 1,
-                    },
-                  },
-                ],
-              },
-            ]}
-            role="application"
-            ariaLabel="Nivo bar chart demo"
-            barAriaLabel={(e) => { return `${e.id}: ${e.formattedValue} in x: ${e.indexValue}`; }}
-          />
+          
         </div>
         <Button
           sx={{ m: 1 }}
